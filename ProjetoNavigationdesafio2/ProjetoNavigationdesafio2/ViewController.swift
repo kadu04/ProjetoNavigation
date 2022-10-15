beeb6d463d85a13e90e1fa27ffe38a050a2c60ec
Ricardo Massaki <ricardo29puruba@gmail.com>
2022-10-15 16:41:24 -0300

    alteracao no codigo do desafio02

--- ProjetoNavigationdesafio2/ProjetoNavigationdesafio2/ViewController.swift
+++ ProjetoNavigationdesafio2/ProjetoNavigationdesafio2/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tappedGoScreen02Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "GoScreen02", sender: nil)
+    }
 }
 
