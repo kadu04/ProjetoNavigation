93fd69921bcef4f54b87b172a03b926504769a26
Ricardo Massaki <ricardo29puruba@gmail.com>
2022-10-16 14:57:26 -0300

    criado uma navigationController para chamar uma tela de maneiraa 100 por cento programatica sem reference e sem segue

---
+++ ProjetoNavigationdesafio8/ProjetoNavigationdesafio6/ProjetoNavigationdesafio6/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ProjetoNavigationdesafio6
+//
+//  Created by Ricardo Massaki on 16/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedGoScreen02Button(_ sender: UIButton) {
+        //     // exibindo modal de maneira programatica
+                //let vc = UIStoryboard(name: "Tela02", bundle: nil).instantiateViewController(withIdentifier: "Tela02")
+                //as? Tela02
+        //        // o present ele exibe a MODAL
+                //present(vc ?? UIViewController(), animated: true)
+                
+                // exibindo NAVIGATION de maneira programatica
+                let vc = UIStoryboard(name: "Tela02", bundle: nil).instantiateViewController(withIdentifier: "Tela02") as? Tela02
+                // o pushViewController ele exibe a tela da controladora
+                navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+            }
+    
+    
+    
+    @IBAction func tappedGoScreen03Button(_ sender: UIButton) {
+    }
+    
+}
+
